9e4d27d4b77c12fac2bbca083dc91d4158f65bc3
Denis Mordovchenkov <denisbabichev@gmail.com>
2025-10-12 18:17:24 +0300

    rewrote tips.js from alert to html render

--- 023-demo-tip-calculator-bootstrap/solution/tips.js
+++ 023-demo-tip-calculator-bootstrap/solution/tips.js
@@ -1,16 +1,40 @@
-let isVisible = false;
-let tips = document.getElementById('tipsPerPeople');
+let rangeValue = document.getElementById('rangeValue');
+let input = document.getElementById('tipsRange');
+let tipsInputValue = input.value;
 
+input.addEventListener('input', (e) => {
+    rangeValue.textContent = `Чаевые ${e.target.value}%`;
+    tipsInputValue = e.target.value;
+});
+let personsInput = document.getElementById('persons');
+let personsInputValue;
+personsInput.addEventListener('input', (e) => {
+    personsInputValue = e.target.value;
+});
+let billInput = document.getElementById('bill');
+let billInputValue;
+billInput.addEventListener('input', (e) => {
+    billInputValue = e.target.value;
+});
 
+let textBill = document.getElementById('textBill');
+let textTips = document.getElementById('textTips');
 
-let changeVisibility = function () {
-    console.log(tips)
-    if (isVisible) {
-        tips.className.remove('.visibility')
-        isVisible = false
-    } else {
-        tips.className.add('.visibility')
-        isVisible = true
-
+let countButton = document.getElementById('countButton');
+countButton.addEventListener('click', () => {
+    if (personsInputValue < 1 || billInputValue <= 0) {
+        textBill.textContent = `Количество человек не может быть меньше 1, а сумма счета 0 или меньше`
+        textTips.textContent = ''
+    } else if (personsInputValue === undefined || billInputValue === undefined) {
+        textBill.textContent = `Вы должны указать количество человек и сумму счёта`
+        textTips.textContent = ''
     }
-}
+
+    else {
+        textBill.textContent = `Сумма счёта по ${Math.round(billInputValue / personsInputValue)} руб. на каждого`;
+        textTips.textContent = `Чаевых по ${Math.ceil((tipsInputValue / 100) * billInputValue / personsInputValue)} руб. с человека`;
+    }
+
+})
+
+
